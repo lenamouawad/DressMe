6c72baf4f2e5ee48d83d20e1f7d2233498962ce7
Lena Mouawad <lena.mouawad@hotmail.com>
2022-03-14 09:55:56 +0100

    Association articles style

--- Services/TenueService.cs
+++ Services/TenueService.cs
@@ -65,6 +65,7 @@ namespace DressMe.Services
             Random random;
             int index;
 
+            // Récupérer tous les hauts convenables
             // Haut by weather
             List<Haut> hautsByMeteo = new List<Haut> { };
             hautsByMeteo = hautServ.FindHautByMeteo(meteo);
@@ -72,19 +73,6 @@ namespace DressMe.Services
             {
                 hautsByMeteo = hautServ.GetAllHauts();
             }
-            // Veste by weather
-            List<Haut> vestesByMeteo = new List<Haut> { };
-            if (meteo == EnumMeteo.frais || meteo == EnumMeteo.froid)
-            {
-                vestesByMeteo = hautServ.FindVesteByMeteo(meteo);
-            }
-            // Bas by weather
-            List<Bas> basByMeteo = basServ.FindBasByMeteo(meteo);
-            if (basByMeteo.Count == 0)
-            {
-                basByMeteo = basServ.FindAll();
-            }
-
             // Haut By type
             List<Haut> hautsByType = new List<Haut>() { };
             hautsByType = hautServ.FilterByType(hautsByMeteo, type);
@@ -95,6 +83,14 @@ namespace DressMe.Services
             else
             {
                 hauts = hautsByType;
+            }
+
+            // Récupérer toutes les vestes convenables
+            // Veste by weather
+            List<Haut> vestesByMeteo = new List<Haut> { };
+            if (meteo == EnumMeteo.frais || meteo == EnumMeteo.froid)
+            {
+                vestesByMeteo = hautServ.FindVesteByMeteo(meteo);
             }
             // Vestes by type
             List<Haut> vestesByType = new List<Haut>() { };
@@ -110,6 +106,14 @@ namespace DressMe.Services
                     vestes = vestesByType;
                 }
             }
+
+            // Récupérer tous les bas convenables
+            // Bas by weather
+            List<Bas> basByMeteo = basServ.FindBasByMeteo(meteo);
+            if (basByMeteo.Count == 0)
+            {
+                basByMeteo = basServ.FindAll();
+            }
             // Bas by type
             List<Bas> basByType = new List<Bas>() { };
             basByType = basServ.FilterByType(basByMeteo, type);
@@ -123,6 +127,8 @@ namespace DressMe.Services
             }
 
             // Tenue
+            AssocierArticles(hauts, vestes, bas);
+
             random = new Random();
             index = random.Next(hauts.Count);
             tenue.hautId = (hauts[index].Id);
@@ -147,6 +153,72 @@ namespace DressMe.Services
             List<Tenue> tenues = repo.AddTenue(tenue);
 
             return tenue;
+        }
+
+        public void AssocierArticles(List<Haut> hauts, List<Haut> vestes, List<Bas> bas)
+        {
+            // Vaut mieux faire en sorte qu'on commence par celui dont on a le moins de choix
+            // Si cest la veste on fait veste -> pantalon meme couleure ou couleur classique, puis on fait haut ?
+            // Si cest pantalon on fait comment ?
+            // Si cest haut faut encore ajouter le pantalon...
+            Random random = new Random();
+            int index = random.Next(hauts.Count);
+
+            Haut haut = hauts[index];
+
+            bool hasMotifHaut = (haut.Motifs != Motifs.pasDeMotifs);
+            bool isHautManyColors = (haut.Couleur.Count > 1);
+
+            if (vestes.Count != 0)
+            {
+                List<Haut> vestesCompatibles = new List<Haut>() { };
+
+                // Exemple : Haut: blanc, rose et jaune - Veste : une couleur parmi les trois ou une couleur classique
+                if (isHautManyColors)
+                {
+                    foreach (Haut veste in vestes)
+                    {
+                        bool isVesteManyColors = (veste.Couleur.Count > 1);
+                        bool hasMotifVeste = (veste.Motifs != Motifs.pasDeMotifs);
+                        // Si le haut a plusieurs couleurs, la veste doit avoir une seule couleur : cette couleur peut etre une des couleurs du haut ou une couleur classique
+                        if (!isVesteManyColors && hasMotifVeste != hasMotifHaut || hasMotifVeste == hasMotifHaut == false && (haut.Couleur.Contains(veste.Couleur.First()) || (IsCouleurClassique(veste.Couleur.First()))))
+                        {
+                            vestesCompatibles.Add(veste);
+                        }
+                    }
+                }
+                // Exemple : Haut: rouge - Veste : une couleur classique ou une veste multicolore avec du rouge dedans
+                if (!isHautManyColors && !IsCouleurClassique(haut.Couleur.First()))
+                {
+                    foreach (Haut veste in vestes)
+                    {
+                        bool isVesteManyColors = (veste.Couleur.Count > 1);
+                        bool hasMotifVeste = (veste.Motifs != Motifs.pasDeMotifs);
+
+                        // Si le haut a une seule couleur pas classique je met une veste avec plusieurs couleurs contenant cette couleur ou une veste de couleur classique
+                        if (hasMotifVeste != hasMotifHaut || hasMotifVeste == hasMotifHaut == false && ((!isVesteManyColors && IsCouleurClassique(veste.Couleur.First())) || (isVesteManyColors && veste.Couleur.Contains(haut.Couleur.First()))))
+
+                            if ((!isVesteManyColors && IsCouleurClassique(veste.Couleur.First()) && hasMotifVeste != hasMotifHaut))
+                            {
+                                vestesCompatibles.Add(veste);
+                            }
+                        if (isVesteManyColors && veste.Couleur.Contains(haut.Couleur.First()) && hasMotifVeste != hasMotifHaut || hasMotifVeste == hasMotifHaut == false)
+                        {
+                            vestesCompatibles.Add(veste);
+                        }
+                    }
+                }
+                // Cas haut couleur classique, veste peut etre multicolore ou pas, couleures classiques ou pas.. pas besoin de traiter ce cas
+
+            }
+
+            //List<Bas> basCompatibles = hasMotifHaut ? bas.Where(bas => bas.Motifs == Motifs.pasDeMotifs).ToList() : bas;
+        }
+
+        public bool IsCouleurClassique(Couleur couleur)
+        {
+            List<Couleur> couleursClassiques = new List<Couleur>() { Couleur.noir, Couleur.blanc, Couleur.beige, Couleur.gris, Couleur.denim };
+            return couleursClassiques.Contains(couleur);
         }
 
         /// <summary>
